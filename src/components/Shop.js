6f90c80d17ccdf5b7a3c3167e54ae6cf36d71a7d
CowboyAragorn <jacobman0313@gmail.com>
2022-11-10 18:57:42 -0500

    feat: add array of objects for store items, map that array onto shop page, display image, name, and price

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,13 +1,52 @@
 import React from "react";
 import styled from "styled-components";
+import shopItems from "./shopItems";
 
 const ShopWrapper = styled.div`
+  display: flex;
+  flex-grow: 1;
   height: ${(props) => props.theme.sizing.mainHeight};
-  background-color: lightgoldenrodyellow;
+  background-color: ${(props) => props.theme.colors.offWhite};
+  background-color: white;
+  overflow: scroll;
+`;
+
+const ShopItemsWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  align-content: center;
+  justify-items: center;
+  justify-self: center;
+`;
+
+const ItemContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 300px;
+  margin: 0;
+`;
+
+const ItemImage = styled.img`
+  width: 200px;
+  height: 150px;
 `;
 
 const Shop = () => {
-  return <ShopWrapper />;
+  return (
+    <ShopWrapper>
+      <ShopItemsWrapper>
+        {shopItems.map((item) => {
+          return (
+            <ItemContainer>
+              <ItemImage src={item.img}></ItemImage>
+              <div>{item.name}</div>
+              <div>{item.price}</div>
+            </ItemContainer>
+          );
+        })}
+      </ShopItemsWrapper>
+    </ShopWrapper>
+  );
 };
 
 export default Shop;
---
+++ src/components/shopItems.js
@@ -0,0 +1,44 @@
+const shopItems = [
+  {
+    name: "Soto Amicus Cookset Combo",
+    price: 45,
+    img: require("../images/cookset.jpeg"),
+  },
+  {
+    name: "Granite Gear Crown 2 Pack",
+    price: 250,
+    img: require("../images/pack.jpg"),
+  },
+  {
+    name: "Frogg Toggs Classic Rain Jacket",
+    price: 35,
+    img: require("../images/rainJacket.jpg"),
+  },
+  {
+    name: "Ursack Major Bear Sack - 10 Liters",
+    price: 100,
+    img: require("../images/bearSack.jpeg"),
+  },
+  {
+    name: "Katadyn BeFree 1.0: Water Filter Bottle",
+    price: 50,
+    img: require("../images/waterFilter.jpeg"),
+  },
+  {
+    name: "Klymit Static V Sleeping Pad",
+    price: 60,
+    img: require("../images/sleepPad.jpg"),
+  },
+  {
+    name: "Magma 15 Sleeping Bag",
+    price: 400,
+    img: require("../images/sleepBag.jpeg"),
+  },
+  {
+    name: "Quarter Dome SL 1 Tent",
+    price: 230,
+    img: require("../images/tent.jpeg"),
+  },
+];
+
+export default shopItems;
