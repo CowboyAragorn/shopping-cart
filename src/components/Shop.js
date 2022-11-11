5f3c87da22548b05d308ee73c9d503ca1d83facd
Jacob Potter <94807302+CowboyAragorn@users.noreply.github.com>
2022-11-11 10:51:32 -0500

    feat: add store page with store items
    * feat: add array of objects for store items, map that array onto shop page, display image, name, and price
    
    * style: put store items in distinguishingly colored containers, grid, scroll when overflowing
    
    * style: change shop format from grid to flex

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,13 +1,95 @@
 import React from "react";
 import styled from "styled-components";
+import shopItems from "./shopItems";
 
 const ShopWrapper = styled.div`
+  display: flex;
+  flex-grow: 1;
   height: ${(props) => props.theme.sizing.mainHeight};
-  background-color: lightgoldenrodyellow;
+  height: 100%;
+  width: 100%;
+  background-color: ${(props) => props.theme.colors.offWhite};
+  padding-top: 25px;
+  padding-bottom: 25px;
+`;
+
+const ShopItemsGrid = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+  background-color: ${(props) => props.theme.colors.offWhite};
+  gap: 100px;
+  align-items: center;
+  justify-content: center;
+
+  /* display: grid;
+  width: 100%;
+  background-color: ${(props) => props.theme.colors.offWhite};
+  grid-template-columns: 1fr 1fr 1fr;
+  align-content: center;
+  justify-items: center;
+  justify-self: center;
+  row-gap: 100px; */
+`;
+
+const ItemContainer = styled.div`
+  display: flex;
+  background-color: white;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+  width: 350px;
+  border: 1pt lightgrey solid;
+  padding: 15px;
+  margin: 0;
+  border-radius: 10px;
+`;
+const ItemImage = styled.img`
+  width: 350px;
+  height: 450px;
+  margin: 0;
+`;
+const ItemName = styled.p`
+  font-size: 1.5rem;
+  margin: 0;
+`;
+const ItemPrice = styled.p`
+  font-size: 1.5rem;
+  margin: 0;
+`;
+
+const StyledButton = styled.button`
+  padding: 12px;
+  width: 60%;
+  font-size: 1.1rem;
+  border-radius: 15px;
+  border: none;
+  background-color: ${(props) => props.theme.colors.middleGreen};
+  color: black;
+  font-weight: bold;
+  &:hover {
+    cursor: pointer;
+    background-color: #356736;
+  }
 `;
 
 const Shop = () => {
-  return <ShopWrapper />;
+  return (
+    <ShopWrapper>
+      <ShopItemsGrid>
+        {shopItems.map((item) => {
+          return (
+            <ItemContainer>
+              <ItemName>{item.name}</ItemName>
+              <ItemImage src={item.img}></ItemImage>
+              <ItemPrice>${item.price}</ItemPrice>
+              <StyledButton>Add to Cart</StyledButton>
+            </ItemContainer>
+          );
+        })}
+      </ShopItemsGrid>
+    </ShopWrapper>
+  );
 };
 
 export default Shop;
---
+++ src/components/shopItems.js
@@ -0,0 +1,44 @@
+const shopItems = [
+  {
+    name: "Soto Amicus Cookset Combo",
+    price: 44.99,
+    img: require("../images/cookset.jpeg"),
+  },
+  {
+    name: "Granite Gear Crown 2 Pack",
+    price: 249.99,
+    img: require("../images/pack.jpg"),
+  },
+  {
+    name: "Frogg Toggs Classic Rain Jacket",
+    price: 34.99,
+    img: require("../images/rainJacket.jpg"),
+  },
+  {
+    name: "Ursack Major Bear Sack - 10 Liters",
+    price: 99.99,
+    img: require("../images/bearSack.jpeg"),
+  },
+  {
+    name: "Katadyn BeFree 1.0: Water Filter Bottle",
+    price: 49.99,
+    img: require("../images/waterFilter.jpeg"),
+  },
+  {
+    name: "Klymit Static V Sleeping Pad",
+    price: 59.99,
+    img: require("../images/sleepPad.jpg"),
+  },
+  {
+    name: "Magma 15 Sleeping Bag",
+    price: 399.99,
+    img: require("../images/sleepBag.jpeg"),
+  },
+  {
+    name: "Quarter Dome SL 1 Tent",
+    price: 229.99,
+    img: require("../images/tent.jpeg"),
+  },
+];
+
+export default shopItems;
